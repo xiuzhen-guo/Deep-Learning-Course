dbb66027be36063e126b3e13ed617c0ac5254a95
xiuzhen-guo <92450513+xiuzhen-guo@users.noreply.github.com>
2023-03-07 07:57:12 +0800

    Update 00_0.前言.py

--- 二MLP/pages/00_0.前言.py
+++ 二MLP/pages/00_0.前言.py
@@ -1,12 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.formula.api as smf
-import statsmodels.api as sm
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 
 """ 本章MLP分3次课来讲"""
 """ 本章介绍了机器学习基础和MLP"""
