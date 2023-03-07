3ed7c962cc40103916ff4cc4c28bd1f6de56f55e
xiuzhen-guo <92450513+xiuzhen-guo@users.noreply.github.com>
2023-03-07 08:08:08 +0800

    Update 00_0.前言.py

--- 三卷积CNN/pages/00_0.前言.py
+++ 三卷积CNN/pages/00_0.前言.py
@@ -1,12 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.formula.api as smf
-import statsmodels.api as sm
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 
 """- 本章总共分为三次课来讲。"""
 """- 本章分别介绍了卷积相关概念、卷积网络和卷积的六个应用。"""
