17f976bd7071432c28469cff2516bf88acfb8731
xiuzhen-guo <92450513+xiuzhen-guo@users.noreply.github.com>
2023-03-07 09:18:02 +0800

    Update Presentation.py

--- 六TL与DRL/Presentation.py
+++ 六TL与DRL/Presentation.py
@@ -8,7 +8,7 @@ st.markdown(
     <body>
     <h1></h1>
     <h1></h1>
-    <h1 font="15"><center>Lecture&nbsp4&nbsp&nbsp&nbsp&nbsp六TL与DRL</center></h1>
+    <h1 font="15"><center>Lecture&nbsp6&nbsp&nbsp&nbsp&nbsp六TL与DRL</center></h1>
     </body>
     """,
     unsafe_allow_html=True)
